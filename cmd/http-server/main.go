d7a5f866b46eb3f4df1a220350baa6eb8f56e9b0
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2024-07-11 18:04:41 -0300

    create server

--- cmd/http-server/main.go
+++ cmd/http-server/main.go
@@ -1,5 +1,119 @@
 package main
 
+import (
+	"context"
+	"crypto/tls"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"golang.org/x/net/http2"
+	"golang.org/x/time/rate"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	cookie := http.Cookie{
+		Name:     "session",
+		Value:    "token",
+		Secure:   true,
+		HttpOnly: true,
+	}
+	http.SetCookie(w, &cookie)
+}
+
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "../../static/not-found.html")
+}
+
+var limiter = rate.NewLimiter(1, 3)
+
+func rateLimit(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if limiter.Allow() {
+			http.Error(w, "Too many requests", http.StatusTooManyRequests)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func secHeaders(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("X-Frame-Options", "deny")
+		w.Header().Set("X-XSS-Protection", "1; mode=block")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.Header().Set("Content-Security-Policy", "default-src 'self'")
+		w.Header().Set("Strict-Transport-Security", "max-age=31536000")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func logg(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		userAgent := r.Header.Get("User-Agent")
+		log.Println(r.RemoteAddr, r.Method, r.URL, userAgent)
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
+	fs := http.FileServer(http.Dir("../../static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/hello", helloHandler)
+
+	http.HandleFunc("/", notFoundHandler)
+
+	mainHandler := http.NewServeMux()
+	mainHandler.Handle("/static/", http.StripPrefix("/static/", fs))
+	mainHandler.HandleFunc("/hello", helloHandler)
+	mainHandler.HandleFunc("/", notFoundHandler)
+	handler := rateLimit(secHeaders(logg(mainHandler)))
+
+	tlsConfig := &tls.Config{
+		MinVersion: tls.VersionTLS12,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+		},
+		PreferServerCipherSuites: true,
+		NextProtos:               []string{"h2"},
+	}
+
+	server := &http.Server{
+		Addr:         ":443",
+		Handler:      handler,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+		TLSConfig:    tlsConfig,
+	}
+
+	http2.ConfigureServer(server, &http2.Server{})
+
+	go func() {
+		log.Printf("Starting on https://127.0.0.1:443")
+		if err := server.ListenAndServeTLS("", ""); err != nil {
+			log.Fatalf("Failed to listen and serve: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v", err)
+	}
+
+	log.Println("Server exiting")
 
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/guijoazeiro/golang-httpServer
 
 go 1.21.6
+
+require (
+	golang.org/x/net v0.27.0
+	golang.org/x/time v0.5.0
+)
+
+require golang.org/x/text v0.16.0 // indirect
